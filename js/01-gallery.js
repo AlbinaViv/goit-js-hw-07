1f10376a40dd3c8f3e13072c3030f236b2a0711b
Albina Vivdenko <albinka3322@gmail.com>
2023-11-11 00:11:02 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './galler-items';
 
 console.log(galleryItems);
+
+const liGallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+    return items
+        .map(({ original, preview, description }) =>
+            `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}"
+                    alt="${description}" />
+            </a>
+        </li>`
+        )
+        .join('');
+}
+const addGallaryMarkup = createGalleryMarkup(galleryItems);
+
+liGallery.innerHTML = addGallaryMarkup;
+
+// liGallery.addEventListener('click', () =>
+
+
+
