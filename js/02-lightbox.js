7d6f676d255c962b5d69f6c8a8338cd4bc5d7020
Albina Vivdenko <albinka3322@gmail.com>
2023-11-12 21:33:50 +0100

    task 72

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulEl = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(items) {
+    return items
+        .map(({ original, preview, description }) =>
+            `<li class= "gallery__item" >
+        <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a></li>`
+        ).join('');
+
+};
+
+ulEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+// ulEl.addEventListener('click', imgContainerClick);
+
+// function imgContainerClick(event) {
+//     event.preventDefault();
+
+//     if (event.target.nodeName !== 'IMG') {
+//         return;
+//     }
+
+//     const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`);
+
+//     instance.show();
+
+//     ulEl.addEventListener('keydown', (event) => {
+//         if (event.code === 'Escape') {
+//             instance.close();
+//         }
+//     });
+// }
